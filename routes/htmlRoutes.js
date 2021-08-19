e2ac1a023036a111db228bdc84f5c00067fbab16
namita.shenai@gmail.com <namita.shenai@gmail.com>
2021-08-19 15:10:38 -0700

    Changed the html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,17 +5,17 @@ const router = require('express').Router();
 const path = require('path');
 
 
-router.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname, '../public/index.html'))
 
-});
 
 
 router.get('/notes', (req,res) =>{
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 
 });
+router.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 
+});
 
 
 module.exports = router; 
