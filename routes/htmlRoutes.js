f243325cb24d74909f8321ba415a92f7a6ff04ac
namita.shenai@gmail.com <namita.shenai@gmail.com>
2021-08-19 23:19:07 -0700

    temp changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,7 +12,7 @@ router.get('/notes', (req,res) =>{
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 
 });
-router.get('*', (req,res) =>{
+router.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 
 });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 //Initialize the app and setups a port for the same 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const parrot = process.env.PORT || 3001;
 
 //JSON parsing 
 app.use(express.json());
@@ -13,8 +13,8 @@ app.use(express.static('public'));
 //Setting up routes 
 app.use("/", htmlRoutes); 
 app.use("/api", apiRoutes); 
-console.log(PORT);
+console.log(parrot);
 //LISTEN to Server 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(parrot, () =>
+  console.log(`App listening at http://localhost:${parrot} 🚀`)
 );
